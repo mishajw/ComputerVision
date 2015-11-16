e1e2d897ca173678975244da8b858bbaf916e729
Dom Williams <dwi3774@googlemail.com>
2015-11-16 15:31:20 +0000

    Made matrix generic and added constructor for square matrices

--- src/main/scala/util/Matrix.scala
+++ src/main/scala/util/Matrix.scala
@@ -1,9 +1,25 @@
 package util
 
-class Matrix(matrixWidth: Int, matrixHeight: Int, array: Array[Int]) {
+class Matrix[T](array: Array[T]) {
 
-	def width = matrixWidth
-	def height = matrixHeight
+	private var _width: Int = {
+		val sqrt = Math.sqrt(array.length)
+		if (sqrt % 0 != 0)
+			throw new IllegalArgumentException(s"Array must be square (current length is ${array.length}")
+		sqrt.asInstanceOf[Int]
+	}
+	private var _height = _width
 
-	def get(x: Int, y: Int) = array(matrixWidth * y + x)
+	def this(array: Array[Int], width: Int, height: Int) {
+		this(array)
+		_width = width
+		_height = height
+	}
+
+
+	def width = _width
+
+	def height = _height
+
+	def get(x: Int, y: Int) = array(_width * y + x)
 }
