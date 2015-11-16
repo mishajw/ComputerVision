fed19495d8cf110c60fe79b1578cf039175e0974
Misha Wagner <mishajw@gmail.com>
2015-11-16 13:49:40 +0000

    Util packages added

--- src/main/scala/compvis/ComputerVisionMain.scala
+++ src/main/scala/compvis/ComputerVisionMain.scala
@@ -1,3 +1,8 @@
+package compvis
+
+/**
+ * Created by misha on 16/11/15.
+ */
 object ComputerVisionMain {
 	def main(args: Array[String]) = println("Hon hon hon!")
 }
---
+++ src/main/scala/util/images/RGBToGray.scala
@@ -0,0 +1,5 @@
+package util.images
+
+object RGBToGray {
+
+}
