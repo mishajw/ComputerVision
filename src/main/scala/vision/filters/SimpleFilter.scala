dba6c8ef764f83732be3651ae646dc2f14432e57
Misha Wagner <mishajw@gmail.com>
2015-11-16 16:39:04 +0000

    Added gaussian filter

---
+++ src/main/scala/vision/filters/SimpleFilter.scala
@@ -0,0 +1,8 @@
+package vision.filters
+
+import vision.filters.FilterFactory.Mask
+import vision.util.ImageWrapper
+
+class SimpleFilter(mask: Mask) extends Filter {
+	override def convolute(image: ImageWrapper): ImageWrapper = ???
+}
