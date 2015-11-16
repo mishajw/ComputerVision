76ef9af13dcb9dc40d834a9a010a212e7e65e6af
Misha Wagner <mishajw@gmail.com>
2015-11-16 14:46:07 +0000

    Better directory structure

--- src/main/scala/compvis/ComputerVisionMain.scala
+++ src/main/scala/compvis/ComputerVisionMain.scala
@@ -1,14 +1,10 @@
 package compvis
 
 import _root_.util.images.Image
-import com.typesafe.config.ConfigFactory
 import grizzled.slf4j.Logging
 
-object ComputerVisionMain extends Logging{
+object ComputerVisionMain extends Logging {
 	def main(args: Array[String]) = {
-		info("Starting...")
-
 		val image = new Image("src/main/resources/red.jpg")
-
 	}
 }
