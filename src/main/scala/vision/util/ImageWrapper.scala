2b2ea90180030b525eae41d1abcdac44f8e96c3b
Dom Williams <dwi3774@googlemail.com>
2015-11-16 16:04:36 +0000

    Added default constructor

--- src/main/scala/vision/util/ImageWrapper.scala
+++ src/main/scala/vision/util/ImageWrapper.scala
@@ -5,34 +5,54 @@ import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.{ImageIcon, JLabel, JOptionPane}
 
-class ImageWrapper(path: String) {
+class ImageWrapper extends Cloneable {
 
-	private val image: BufferedImage = {
-		val file = new File(path)
+	private var image: BufferedImage = _
+	private var pixels: Matrix[Int] = _
 
-		// validation
-		if (!file.exists()) {
-			Console.err.println("Input file does not exist")
-			sys.exit(1)
+	private var _width = _
+	private var _height = _
+
+	def width = _width
+
+	def height = _height
+
+	def this(filePath: String) {
+		this()
+
+		image = {
+			val file = new File(filePath)
+
+			// validation
+			if (!file.exists()) {
+				Console.err.println("Input file does not exist")
+				sys.exit(1)
+			}
+
+			val image = ImageIO.read(file)
+
+			val greyImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_BYTE_GRAY)
+			val g = greyImage.createGraphics()
+			g.drawImage(image, 0, 0, null)
+			g.dispose()
+
+			greyImage
 		}
 
-		val image = ImageIO.read(file)
+		pixels = new Matrix(image.getData.getPixels(0, 0, image.getWidth, image.getHeight, null: Array[Int]),
+			image.getWidth, image.getHeight)
 
-		val greyImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_BYTE_GRAY)
-		val g = greyImage.createGraphics()
-		g.drawImage(image, 0, 0, null)
-		g.dispose()
-
-		greyImage
+		_width = image.getWidth
+		_height = image.getHeight
 	}
-
-	private val pixels = new Matrix(image.getData.getPixels(0, 0, image.getWidth, image.getHeight, null: Array[Int]),
-		image.getWidth, image.getHeight)
-
-	val width = pixels.width
-	val height = pixels.height
 
 	def getPixel(x: Int, y: Int): Int = pixels.get(x, y)
 
 	def display() = JOptionPane.showMessageDialog(null, new JLabel(new ImageIcon(image)))
+
+	override def clone(): AnyRef = {
+		val newImage = new ImageWrapper
+
+
+	}
 }
