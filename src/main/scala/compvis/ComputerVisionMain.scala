20b2602307de9440874744ee3ac579192fda9fa8
Misha Wagner <mishajw@gmail.com>
2015-11-16 14:18:24 +0000

    Added logging libraries

--- src/main/scala/compvis/ComputerVisionMain.scala
+++ src/main/scala/compvis/ComputerVisionMain.scala
@@ -1,9 +1,12 @@
 package compvis
 
 import _root_.util.images.Image
+import grizzled.slf4j.Logging
 
-object ComputerVisionMain {
+object ComputerVisionMain extends Logging{
 	def main(args: Array[String]) = {
+		info("Starting...")
+
 		val image = new Image("src/main/resources/red.jpg")
 
 	}
