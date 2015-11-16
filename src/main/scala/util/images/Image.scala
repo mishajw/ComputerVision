4707323af6520099dc00964627702bbf035b7fee
Dom Williams <dwi3774@googlemail.com>
2015-11-16 14:12:18 +0000

    Simple image wrapper for rgb pixel retrieval

--- src/main/scala/util/images/Image.scala
+++ src/main/scala/util/images/Image.scala
@@ -1,13 +1,27 @@
 package util.images
 
-/**
- * Created by misha on 16/11/15.
- */
-class Image {
-	val width = 0
-	val height = 0
+import java.io.File
+import javax.imageio.ImageIO
 
-	def getPixel(x: Int, y: Int): Seq[Int] = {
-		0
+class Image(path: String) {
+
+	private val image = ImageIO.read(new File(path))
+	private val pixels = image.getData.getPixels(0, 0, image.getWidth, image.getHeight, null: Array[Int])
+
+	val width = image.getWidth
+	val height = image.getHeight
+
+	/**
+	  * @return (r, g, b, a)
+	  */
+	def getPixel(x: Int, y: Int): (Int, Int, Int, Int) = {
+		val rgb = image.getColorModel.getRGB(pixels(width * x + y))
+
+		val a = (rgb >> 24) & 0xff
+		val r = (rgb >> 16) & 0xff
+		val g = (rgb >> 8) & 0xff
+		val b = rgb & 0xff
+
+		(r, g, b, a)
 	}
 }
