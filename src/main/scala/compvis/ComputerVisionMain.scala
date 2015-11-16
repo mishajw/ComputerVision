68d2618628f8666441ce3f81b12be18b49766ca8
Misha Wagner <mishajw@gmail.com>
2015-11-16 14:11:26 +0000

    RGB to gray done, but untested until Image class finished

--- src/main/scala/compvis/ComputerVisionMain.scala
+++ src/main/scala/compvis/ComputerVisionMain.scala
@@ -1,8 +1,5 @@
 package compvis
 
-/**
- * Created by misha on 16/11/15.
- */
 object ComputerVisionMain {
 	def main(args: Array[String]) = println("Hon hon hon!")
 }
--- src/main/scala/util/images/RGBToGray.scala
+++ src/main/scala/util/images/RGBToGray.scala
@@ -1,5 +1,13 @@
 package util.images
 
 object RGBToGray {
+	def convert(rgb: Image) {
+		var gray: Array[Int] = Array()
 
+		for (x <- 0 to rgb.height; y <- 0 to rgb.width) {
+			val pixel = rgb.getPixel(x, y)
+
+			gray = gray :+ (pixel.sum / pixel.length)
+		}
+	}
 }
