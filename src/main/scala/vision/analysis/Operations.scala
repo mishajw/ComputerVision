9495984591fad5aabd4e8627f30d5328a984b0f3
Dom Williams <dwi3774@googlemail.com>
2015-12-02 17:42:32 +0000

    Added enums for all operations

---
+++ src/main/scala/vision/analysis/Operations.scala
@@ -0,0 +1,37 @@
+package vision.analysis
+
+object Operations {
+
+	// thresholds of 20, 40, 60, 80, 100
+	val TRANSFORMATIONS = Seq(TransformationIntensity) ++
+		(20 to 100 by 20 map TransformationBinary)
+
+	val EDGE_DETECTORS = Seq(SimpleGradient, Sobel, Roberts, Prewitt)
+
+	// todo noise removals
+
+	abstract sealed class ImageTransformation
+
+	case object TransformationIntensity extends ImageTransformation
+
+	case class TransformationBinary(threshold: Int) extends ImageTransformation
+
+
+	abstract sealed class NoiseRemoval
+
+	case object SimpleMean extends NoiseRemoval
+
+	case class Gaussian(size: Int, standardDeviation: Int) extends NoiseRemoval
+
+
+	abstract sealed class EdgeDetection
+
+	case object SimpleGradient extends EdgeDetection
+
+	case object Sobel extends EdgeDetection
+
+	case object Roberts extends EdgeDetection
+
+	case object Prewitt extends EdgeDetection
+
+}
